9baa1e42cc2390e7bcfb86f9da198f951fc2580d
natakozlova <natabalabanova555@yandex.ru>
2023-05-01 12:45:51 -0700

    Add junit4 tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,7 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import ru.netology.service.CashbackHackService;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
 
